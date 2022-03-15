68e12c53ba3159c87fd870433d58b39764113b7e
carvalhoevandro <evandro.ferreira@startse.com>
2022-03-14 21:30:02 -0300

    feat: ensure remote ath throws invalid credentials error

--- react-tdd/projeto/src/data/usecases/authentication/remote-authentication.spec.ts
+++ react-tdd/projeto/src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,3 +1,5 @@
+import { HttpStatusCode } from "@/data/protocols/http/http-response";
+import { InvalidCredentialsError } from "@/domain/errors/invalid-credential-erro";
 import { mockAuthentication } from "@/domain/test/mock-authentication";
 import { HttpPostClientSpy } from "../../test/mock-http-client";
 import { RemoteAuthentication } from "./remote-authentication";
@@ -31,5 +33,14 @@ describe("RemoteAuthentication", () => {
     const authenticationParams = mockAuthentication();
     await sut.auth(authenticationParams);
     expect(httpPostClientSpy.body).toEqual(authenticationParams);
+  });
+  
+  test("Should throw InvalidCredentailsError if HttpPostClient returns 401", async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.unathorized
+    }
+    const promise = sut.auth(mockAuthentication());
+    await expect(promise).rejects.toThrow(new InvalidCredentialsError());
   });
 });
