fe7f96edb972f1e51178dbc7267764f381e45708
carvalhoevandro <evandro.ferreira@startse.com>
2022-03-22 22:56:55 -0300

    refactor: add index with multiple imports

--- react-tdd/projeto/src/presentation/components/form-status/form-status.tsx
+++ react-tdd/projeto/src/presentation/components/form-status/form-status.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Spinner from "../spinner/spinner";
+import { Spinner } from "../";
 import Styles from "./form-status-styles.scss";
 
 const FormStatus: React.FC = () => {
