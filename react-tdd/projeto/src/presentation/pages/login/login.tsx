c6ef054e7336e607e9eefbd509308e0950e46834
carvalhoevandro <evandro.ferreira@startse.com>
2022-03-22 22:19:01 -0300

    refactor: header and footer components

--- react-tdd/projeto/src/presentation/pages/login/login.tsx
+++ react-tdd/projeto/src/presentation/pages/login/login.tsx
@@ -2,26 +2,36 @@ import React from "react";
 
 import Styles from "./login-styles.scss";
 import Spinner from "@/presentation/components/spinner/spinner";
-import Logo from "@/presentation/components/logo/logo";
+import Header from "@/presentation/components/login-header/login-header";
+import Footer from "@/presentation/components/footer/footer";
 
 const Login: React.FC = () => {
   return (
     <div className={Styles.login}>
-      <header className={Styles.header}>
-        <Logo />
-        <h1>4Dev - Enquetes para Programadores</h1>
-      </header>
+      <Header />
       <form className={Styles.form}>
         <h2>Login</h2>
         <div className={Styles.inputWrap}>
-          <input type="email" name="email" placeholder="Digite seu email" autoComplete="off" />
-          <span  className={Styles.status}>🔴</span>
-        </div>
-        <div className={Styles.inputWrap}>
-          <input type="password" name="password" placeholder="Digite sua senha" autoComplete="off" />
+          <input
+            type="email"
+            name="email"
+            placeholder="Digite seu email"
+            autoComplete="off"
+          />
           <span className={Styles.status}>🔴</span>
         </div>
-        <button className={Styles.submit} type="submit">Entrar</button>
+        <div className={Styles.inputWrap}>
+          <input
+            type="password"
+            name="password"
+            placeholder="Digite sua senha"
+            autoComplete="off"
+          />
+          <span className={Styles.status}>🔴</span>
+        </div>
+        <button className={Styles.submit} type="submit">
+          Entrar
+        </button>
         <span className={Styles.link}>Criar conta</span>
 
         <div className={Styles.errorWrap}>
@@ -29,7 +39,7 @@ const Login: React.FC = () => {
           <span className={Styles.error}>Erro</span>
         </div>
       </form>
-      <footer className={Styles.footer} />
+      <Footer />
     </div>
   );
 };
